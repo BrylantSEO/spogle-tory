6d562624b199d4db45be8bacb19d1e68e068d420
Maciej Kulkowski <mkulkowski@doubledigital.pl>
2026-03-01 15:16:09 +0000

    File changes

--- src/components/SetCard.jsx
+++ src/components/SetCard.jsx
@@ -1,4 +1,4 @@
-export default function SetCard({ set, isActive, onSelect }) {
+export default function SetCard({ set, isActive, onSelect, onDetail }) {
   return (
     <div
       onClick={onSelect}
