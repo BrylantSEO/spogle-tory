740a3a5975b3a8faae734d3c25ebb34f2b511179
Maciej Kulkowski <mkulkowski@doubledigital.pl>
2026-03-04 11:01:49 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,6 +2,28 @@ import { useEffect } from "react";
 
 export default function Layout({ children }) {
   useEffect(() => {
+    // Google Tag Manager
+    (function(w,d,s,l,i){
+      w[l]=w[l]||[];
+      w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+      var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),
+          dl=l!='dataLayer'?'&l='+l:'';
+      j.async=true;
+      j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+      f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-WKK9NMFH');
+
+    // GTM noscript
+    const noscript = document.createElement('noscript');
+    const iframe = document.createElement('iframe');
+    iframe.src = 'https://www.googletagmanager.com/ns.html?id=GTM-WKK9NMFH';
+    iframe.height = '0';
+    iframe.width = '0';
+    iframe.style.cssText = 'display:none;visibility:hidden';
+    noscript.appendChild(iframe);
+    document.body.insertBefore(noscript, document.body.firstChild);
+
     // Meta Pixel
     (function(f,b,e,v,n,t,s){
       if(f.fbq)return;n=f.fbq=function(){n.callMethod?
